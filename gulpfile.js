78718aec2ae48580da34e4be672c14e22f7dcb01
AndreyWetrow <andrnazkem@mail.ru>
2020-03-30 21:28:52 +0700

    after html academy level-1

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@
 
 // npm i jquery-validation
 
+//cmd//cd /d и просто перетащить папку
+
 //npm i -g npm-check-updates - С помощью npm-check-updates можно узнать какие зависимости обновились с момента их установки с помощью npm/bower.
 //Потом, перед иницииализацией - ncu (просто проверка package.json на обновления), если есть что обновлять тогда ncu -u
 
@@ -16,28 +18,28 @@
 //Для сжатия картинок лучше использовать сайт https://tinypng.com/
 //npm init -y - создает пустой package.json с заполненными по умолчанию параметрами
 
-const gulp = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
-const del = require('del');
-const browserSync = require('browser-sync').create();
-const concat = require('gulp-concat');
-const cleanCSS = require('gulp-clean-css');
-const sourcemaps = require('gulp-sourcemaps');
-const gulpif = require('gulp-if');
-var gcmq = require('gulp-group-css-media-queries');
-const uglify = require('gulp-uglify'); 
-var sass = require('gulp-sass');
-const smartgrid = require('smart-grid');
+const gulp = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const del = require("del");
+const browserSync = require("browser-sync").create();
+const concat = require("gulp-concat");
+const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
+const gulpif = require("gulp-if");
+var gcmq = require("gulp-group-css-media-queries");
+const uglify = require("gulp-uglify");
+var sass = require("gulp-sass");
+const smartgrid = require("smart-grid");
 //Конец Smartgrid
-const cheerio = require('gulp-cheerio');
-const imagemin = require('gulp-imagemin');
+const cheerio = require("gulp-cheerio");
+const imagemin = require("gulp-imagemin");
 // const plumber = require('gulp-plumber'); //Для вывода ошибок
 // const coffee = require('gulp-coffee');
-const replace = require('gulp-replace');
-const svgSprite = require('gulp-svg-sprite');
-const svgmin = require('gulp-svgmin');
-const imageminJpegRecompress = require('imagemin-jpeg-recompress');
-const imageminPngquant = require('imagemin-pngquant');
+const replace = require("gulp-replace");
+const svgSprite = require("gulp-svg-sprite");
+const svgmin = require("gulp-svgmin");
+const imageminJpegRecompress = require("imagemin-jpeg-recompress");
+const imageminPngquant = require("imagemin-pngquant");
 const pug = require("gulp-pug");
 const merge = require("merge-stream");
 // const svgo = require('gulp-svgo');
@@ -49,71 +51,79 @@ const merge = require("merge-stream");
 // const isSync = true;
 
 // Второй вариант
-const isDev = (process.argv.indexOf('--dev') !== -1);
+const isDev = process.argv.indexOf("--dev") !== -1;
 const isProd = !isDev;
-const isSync = (process.argv.indexOf('--sync') !== -1);
+const isSync = process.argv.indexOf("--sync") !== -1;
 
 // let cssFiles = [
-  // './node_modules/normalize.css/normalize.css',
+// './node_modules/normalize.css/normalize.css',
 //   './src/css/base.css',
 //   './src/css/other.css',
 //   './src/css/styles.css'
 // ];
 
 let jsFiles = [
-  './src/libs/jquery/jquery.min.js',
-  './src/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
-  'node_modules/simplebar/dist/simplebar.min.js',
-  // './src/libs/slick-carousel/slick/slick.min.js',
+  "./src/libs/jquery/jquery.min.js",
+  "./src/libs/magnific-popup/dist/jquery.magnific-popup.min.js",
+  "node_modules/simplebar/dist/simplebar.min.js",
+  './src/libs/slick-carousel/slick/slick.min.js',
   // './node_modules/wow.js/dist/wow.min.js',
   // './src/libs/simplebar/packages/simplebar/src/simplebar.min.js',
   // './src/libs/jqueryValidation/jquery.validate.min.js',
   // './src/libs/maskedinput/maskedinput.js',
   // './src/libs/noUiSlider/nouislider.min.js',
   // './src/libs/wNumb/wNumb.min.js',
-  // './src/libs/jquery-mmenu/jquery.mmenu.min.all.js', 
-  // './src/libs/menuStyleSlider/js/index.js', 
-  // './src/libs/OwlCarousel/OwlCarousel2-2.3.4/dist/owl.carousel.min.js', 
-  // './src/libs/jQuery.equalHeights/jquery.equalheights.js', 
-  // './src/libs/fotorama/fotorama.js', 
-  // './src/libs/selectize/dist/js/standalone/selectize.min.js', 
-  './src/js/script.js'
+  // './src/libs/jquery-mmenu/jquery.mmenu.min.all.js',
+  // './src/libs/menuStyleSlider/js/index.js',
+  // './src/libs/OwlCarousel/OwlCarousel2-2.3.4/dist/owl.carousel.min.js',
+  // './src/libs/jQuery.equalHeights/jquery.equalheights.js',
+  // './src/libs/fotorama/fotorama.js',
+  // './src/libs/selectize/dist/js/standalone/selectize.min.js',
+  "./src/js/script.js"
 ];
 
 function clear() {
-  return del('build/*');
-} 
+  return del("build/*");
+}
 
 function styles() {
-  return gulp.src('./src/scss/style.scss')
-    .pipe(gulpif(isDev, sourcemaps.init()))
-    .pipe(sass())
-    // .pipe(concat('styles.css'))
-    // .on('error', console.error.bind(console))
+  return (
+    gulp
+      .src("./src/scss/style.scss")
+      .pipe(gulpif(isDev, sourcemaps.init()))
+      .pipe(sass())
+      // .pipe(concat('styles.css'))
+      // .on('error', console.error.bind(console))
 
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['>0.1%'],
-      cascade: false
-    }))
-    //Из-за gcmq() плохо работает sourseMap, наверное луче использовать в конце верстки
-    // .pipe(gcmq()) 
-    .pipe(gulpif(isProd, cleanCSS({ level: 2 })))
-    .pipe(gulpif(isDev, sourcemaps.write()))
+      .pipe(
+        autoprefixer({
+          overrideBrowserslist: [">0.1%"],
+          cascade: false
+        })
+      )
+      //Из-за gcmq() плохо работает sourseMap, наверное луче использовать в конце верстки
+      // .pipe(gcmq())
+      .pipe(gulpif(isProd, cleanCSS({ level: 2 })))
+      .pipe(gulpif(isDev, sourcemaps.write()))
 
-    .pipe(gulp.dest('./build/css'))
-    .pipe(gulpif(isSync, browserSync.stream()));
+      .pipe(gulp.dest("./build/css"))
+      .pipe(gulpif(isSync, browserSync.stream()))
+  );
 }
 
 function scripts() {
-  return gulp.src(jsFiles)
-    .pipe(gulpif(isDev, sourcemaps.init()))
-    .pipe(concat('all.js'))
-    // .pipe(uglify({
-    //   toplevel: true
-    // }))
-    .pipe(gulpif(isDev, sourcemaps.write()))
-    .pipe(gulp.dest('./build/js'))
-    .pipe(gulpif(isSync, browserSync.stream()))
+  return (
+    gulp
+      .src(jsFiles)
+      .pipe(gulpif(isDev, sourcemaps.init()))
+      .pipe(concat("all.js"))
+      // .pipe(uglify({
+      //   toplevel: true
+      // }))
+      .pipe(gulpif(isDev, sourcemaps.write()))
+      .pipe(gulp.dest("./build/js"))
+      .pipe(gulpif(isSync, browserSync.stream()))
+  );
 }
 
 // function allimg() {
@@ -122,69 +132,84 @@ function scripts() {
 //     .pipe(gulpif(isSync, browserSync.stream()))
 // }
 
-
 function images() {
-  return gulp.src('./src/img/**/*.{png,jpg}')
-    .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.mozjpeg({quality: 75, progressive: true}),
-      imagemin.optipng({optimizationLevel: 5})
-    ]))
-    .pipe(gulp.dest('./build/img'))
-    .pipe(gulpif(isSync, browserSync.stream()))
+  return gulp
+    .src("./src/img/**/*.{png,jpg}")
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        // imagemin.mozjpeg({ quality: 75, progressive: true }), выдет ошибку
+        imagemin.optipng({ optimizationLevel: 5 })
+      ])
+    )
+    .pipe(gulp.dest("./build/img"))
+    .pipe(gulpif(isSync, browserSync.stream()));
 }
 
 function svg() {
-  return gulp.src('./src/img/sprite/**/*.svg')
-    .pipe(svgmin({
-      js2svg: {
-        pretty: true
-      }
-    }))
-    .pipe(cheerio({
-      run: function($) {
-        $('[fill]').removeAttr('fill');
-        $('[stroke]').removeAttr('stroke');
-        $('[style]').removeAttr('style');
-      },
-      parserOptions: { xmlMode: true }
-    }))
-    .pipe(replace('&gt;', '>'))
-    // build svg sprite
-    .pipe(svgSprite({
-      mode: {
-        symbol: {
-          sprite: "sprite.svg" 
-        }
-      }
-    })) 
-    .pipe(gulp.dest('./build/img/'))
-};
+  return (
+    gulp
+      .src("./src/img/sprite/**/*.svg")
+      .pipe(
+        svgmin({
+          js2svg: {
+            pretty: true
+          }
+        })
+      )
+      .pipe(
+        cheerio({
+          run: function($) {
+            $("[fill]").removeAttr("fill");
+            $("[stroke]").removeAttr("stroke");
+            $("[style]").removeAttr("style");
+          },
+          parserOptions: { xmlMode: true }
+        })
+      )
+      .pipe(replace("&gt;", ">"))
+      // build svg sprite
+      .pipe(
+        svgSprite({
+          mode: {
+            symbol: {
+              sprite: "sprite.svg"
+            }
+          }
+        })
+      )
+      .pipe(gulp.dest("./build/img/"))
+  );
+}
 
 function totalSvg() {
-  return gulp.src('./src/img/totalSvg/**/*.svg')
-    .pipe(gulp.dest('./build/img'))
-    .pipe(gulpif(isSync, browserSync.stream()))
+  return gulp
+    .src("./src/img/totalSvg/**/*.svg")
+    .pipe(gulp.dest("./build/img"))
+    .pipe(gulpif(isSync, browserSync.stream()));
 }
 
 function html() {
-  return gulp.src('./src/*.html')
-    .pipe(gulp.dest('./build'))
-    .pipe(gulpif(isSync, browserSync.stream()))
+  return gulp
+    .src("./src/*.html")
+    .pipe(gulp.dest("./build"))
+    .pipe(gulpif(isSync, browserSync.stream()));
 }
 
 function htmlPug() {
   return gulp
     .src("./src/pug/*.pug")
-    .pipe(pug({
-      pretty: true
-    }))
+    .pipe(
+      pug({
+        pretty: true
+      })
+    )
     .pipe(gulp.dest("./build"))
     .pipe(gulpif(isSync, browserSync.stream()));
-} 
+}
 
 function clearFonts() {
-  return del('./build/fonts');
+  return del("./build/fonts");
 }
 function fonts() {
   return gulp
@@ -218,26 +243,26 @@ function watch() {
       }
     });
   }
-  gulp.watch('./src/scss/**/*.scss', styles);
-  gulp.watch('./src/libs/**/*', gulp.parallel(styles, scripts));
-  gulp.watch('./src/**/*.html', html);
+  gulp.watch("./src/scss/**/*.scss", styles);
+  gulp.watch("./src/libs/**/*", gulp.parallel(styles, scripts));
+  gulp.watch("./src/**/*.html", html);
   gulp.watch("./src/**/*.pug", htmlPug);
-  gulp.watch('./src/js/**/*.js', scripts);
-  gulp.watch('./src/img/**/*.{png,jpg}', images);
-  gulp.watch('./src/img/sprite/*.svg', svg);
-  gulp.watch('./src/img/totalSvg/**/*.svg', totalSvg);
-  gulp.watch('./src/fonts/**/*', gulp.series(clearFonts, fonts));
+  gulp.watch("./src/js/**/*.js", scripts);
+  gulp.watch("./src/img/**/*.{png,jpg}", images);
+  gulp.watch("./src/img/sprite/*.svg", svg);
+  gulp.watch("./src/img/totalSvg/**/*.svg", totalSvg);
+  gulp.watch("./src/fonts/**/*", gulp.series(clearFonts, fonts));
   // gulp.watch('./src/img/**/*.{png,jpg}', allimg);
   // gulp.watch('./src/img/**/*.svg', gulp.series(clearCopyResources, copyResources, svg));
   // gulp.watch('./src/*.php', gulp.series(clearCopyResources, copyResources));
   // gulp.watch('./src/fonts/**/*', gulp.series(clearCopyResources, copyResources));
-  gulp.watch('./smartgrid.js', grid);
+  gulp.watch("./smartgrid.js", grid);
 }
 
 function grid(done) {
-  delete require.cache[require.resolve('./smartgrid.js')];
-  let settings = require('./smartgrid.js');
-  smartgrid('./src/scss', settings);
+  delete require.cache[require.resolve("./smartgrid.js")];
+  let settings = require("./smartgrid.js");
+  smartgrid("./src/scss", settings);
 
   // settings.offset = '3.1%';
   // settings.filename = 'smart-grid-per';
@@ -248,9 +273,10 @@ function grid(done) {
 let build = gulp.series(
   clear,
   gulp.parallel(htmlPug, styles, scripts, images, svg, totalSvg, fonts)
+  //для html// gulp.parallel(html, styles, scripts, images, svg, totalSvg, fonts),
 );
 
-gulp.task('build', gulp.series(grid, build));
-gulp.task('watch', gulp.series(build, watch));
+gulp.task("build", gulp.series(grid, build));
+gulp.task("watch", gulp.series(build, watch));
 
-gulp.task('grid', grid);
+gulp.task("grid", grid);
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,20 @@
 $(function() {
+  $('.header-slider').slick({
+    dots: true,
+    // appendDots: $(".test"),
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    arrows: false
+  });
+
+  $('.conditions__list-item').click(function() {
+    var tabName = $(this).attr('show-tab');
+    $(this).addClass('conditions__list-item--active').siblings().removeClass('conditions__list-item--active');
+    $('.tabs-content.' + tabName).addClass('tab--active').siblings().removeClass('tab--active');
+  });
+
+
   var popupLink = function() {
     $('.js-popup-link').magnificPopup({
       showCloseBtn: false,
@@ -10,8 +26,66 @@ $(function() {
     // });
   };
 
+  $('.catalog__btn').on('click', function() {
+    $('.full-nav').toggleClass('full-nav--active');
+    $('.catalog__block').toggleClass('catalog__block--active');
+  });
+
+  let writeUs = document.querySelector('.write-us');
+  let overlay = document.querySelector('.overlay');
+  let send = document.querySelector('.popup.send');
+  let sendCloseBtn = document.querySelector('.popup .close__btn');
+  let name = send.querySelector('[name=name]');
+  let email = send.querySelector('[name=email]');
+  let form = send.querySelector('form');
+  let login = localStorage.getItem('login');
+
+
+  function popupSend() {
+    send.classList.remove('send--active');
+    overlay.classList.remove('overlay--active');
+  }
+
+  writeUs.addEventListener('click', function(e) {
+    e.preventDefault();
+    send.classList.add('send--active');
+    overlay.classList.add('overlay--active');
+    name.focus();
+    if (login) {
+      name.value = login;
+      email.focus();
+    }
+  });
+
+  sendCloseBtn.addEventListener('click', function(e) {
+    popupSend();
+  });
+  overlay.addEventListener('click', function(e) {
+    popupSend();
+  });
+  form.addEventListener('submit', function(e) {
+
+    if (!email.value || !name.value) {
+      e.preventDefault();
+      console.log(" не ввел");
+    } else {
+      localStorage.setItem('login', name.value);
+    }
+  });
+  window.addEventListener('keydown', function(e) {
+    if (e.keyCode === 27) {
+      if (send.classList.contains('send--active')) {
+        e.preventDefault();
+        popupSend();
+      } 
+    }
+  });
+  console.log(document.documentElement);
+
   popupLink();
 });
+
+
 
 $(window).on("load", function() {
   $(".sk-fading-circle").fadeOut();
