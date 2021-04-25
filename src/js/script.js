cfa0027cc334dd4d3e3fdc30a2c0f600c886c80a
Your Name <you@example.com>
2021-04-25 11:01:18 +0700

    After SuperSlider

--- src/js/script.js
+++ src/js/script.js
@@ -7,23 +7,47 @@
 // }).then(() => {
 //   console.log(111111);
 // });
-
 // import "core-js/features/promise"; не помню для чего
 import "./addClassWebp.js";
+// import MicroModal from "micromodal";
 
+// ***********import JQUERY*********
+// $(function(){});
 // import $ from "jquery";
+// window.jQuery = $;
+// window.$ = $;
+//**********************************
+// ***********import GSAP библиотека анимаций*********
+// import { gsap, Power2 } from "gsap";
+//**********************************
+// ***********import SWIPER*********
+// import Swiper from "swiper/bundle";
+// import {
+//   Swiper,
+//   Parallax,
+//   Mousewheel,
+//   Controller,
+//   Pagination,
+//   Scrollbar,
+//   Navigation,
+// } from "swiper";
+// Swiper.use([
+//   Parallax,
+//   Mousewheel,
+//   Controller,
+//   Pagination,
+//   Scrollbar,
+//   Navigation,
+// ]);
+//**********************************
+
 // import "./ion.rangeSlider.min.js";
 // import "./jquery.formstyler.min.js";
 // import "slick-carousel";
 // import RateYo from "rateyo";
 
 // import noUiSlider from "nouislider";
-// import Swiper from "swiper/bundle";
 // import wNumb from "wNumb";
-
-// =============Jquery start================
-// $(function(){});
-// =============Jquery end==================
 
 // import './assets/css/main.css';
 
@@ -31,5 +55,14 @@ import "./addClassWebp.js";
 // import 'swiper/swiper-bundle.css';
 
 window.addEventListener("load", function () {
-   console.log(123);
+  // Modal
+  // MicroModal.init({
+  //   openTrigger: "data-micromodal-open",
+  //   closeTrigger: "data-micromodal-close",
+  //   disableFocus: true,
+  //   disableScroll: true,
+  //   awaitOpenAnimation: true,
+  //   awaitCloseAnimation: true,
+  // });   
+ 
 });
